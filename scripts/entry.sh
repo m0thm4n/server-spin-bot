3f77238becc81e8d1580affd2a83c9f838e90f35
Nathan Moritz <nateim3@gmail.com>
2020-09-23 17:05:33 -0400

    Output redirected

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -3,7 +3,10 @@ bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
 				+force_install_dir "${STEAMAPPDIR}" \
 				+app_update "${STEAMAPPID}" \
 				+quit
-sed -i -e 's/Port=21114/'"Port=${RCONPORT}"'/g' "${STEAMAPPDIR}/SEGame/ServerConfig/Rcon.cfg"
+
+# Change rcon port on first launch, because the default config overwrites the commandline parameter (you can comment this out if it has done it's purpose)
+sed -i -e 's/Port=21114/'"Port=${RCONPORT}"'/g' > "${STEAMAPPDIR}/SEGame/ServerConfig/Rcon.cfg"
+
 bash "${STEAMAPPDIR}/SeGameServer.sh" \
 			Port="${PORT}" \
 			QueryPort="${QUERYPORT}" \
