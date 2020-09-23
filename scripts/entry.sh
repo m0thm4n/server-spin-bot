1415d528c5885532c0fe6124180e69490c538979
Nathan Moritz <nateim3@gmail.com>
2020-09-23 17:00:07 -0400

    Removed newline from shell script

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -3,9 +3,7 @@ bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
 				+force_install_dir "${STEAMAPPDIR}" \
 				+app_update "${STEAMAPPID}" \
 				+quit
-
 sed -i -e 's/Port=21114/'"Port=${RCONPORT}"'/g' "${STEAMAPPDIR}/SEGame/ServerConfig/Rcon.cfg"
-
 bash "${STEAMAPPDIR}/SeGameServer.sh" \
 			Port="${PORT}" \
 			QueryPort="${QUERYPORT}" \
