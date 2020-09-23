53f02d31376e30e67e969fbf7982ffb43ee889d7
Nathan Moritz <nateim3@gmail.com>
2020-09-23 18:05:10 -0400

    Wine now running Space Engineers Server

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
+alias seserv = "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
 				+force_install_dir "${STEAMAPPDIR}" \
 				+app_update "${STEAMAPPID}" \
 				+quit
 
 # Change rcon port on first launch, because the default config overwrites the commandline parameter (you can comment this out if it has done it's purpose)
-sed -i -e 's/Port=21114/'"Port=${RCONPORT}"'/g' > "${STEAMAPPDIR}/SEGame/ServerConfig/Rcon.cfg"
+sed -i -e 's/Port=21114/'"Port=${RCONPORT}"'/g' "${STEAMAPPDIR}/SEGame/ServerConfig/Rcon.cfg"
 
-bash "${STEAMAPPDIR}/SeGameServer.sh" \
-			Port="${PORT}" \
-			QueryPort="${QUERYPORT}" \
-			RCONPORT="${RCONPORT}" \
-			FIXEDMAXPLAYERS="${FIXEDMAXPLAYERS}" \
-			RANDOM="${RANDOM}"
+wine "${STEAMAPPDIR}/SpaceEngineersDedicated.exe"
+bash 	Port="${PORT}" \
+		QueryPort="${QUERYPORT}" \
+		RCONPORT="${RCONPORT}" \
+		FIXEDMAXPLAYERS="${FIXEDMAXPLAYERS}" \
+		RANDOM="${RANDOM}"
+
+# bash "${STEAMAPPDIR}/SeGameServer.sh" \
+# 			Port="${PORT}" \
+# 			QueryPort="${QUERYPORT}" \
+# 			RCONPORT="${RCONPORT}" \
+# 			FIXEDMAXPLAYERS="${FIXEDMAXPLAYERS}" \
+# 			RANDOM="${RANDOM}"
