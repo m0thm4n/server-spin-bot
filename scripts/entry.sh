107aeaa26d8b130ba4f377456766de1191628812
Nathan Moritz <nateim3@gmail.com>
2020-09-24 12:14:06 -0400

    Added quotes to string in entry script

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous +force_install_dir "${STEAMAPPDIR}" +app_update "${STEAMAPPID}" +quit
+bash "${STEAMCMDDIR}/steamcmd.sh +login anonymous +force_install_dir "${STEAMAPPDIR}" +app_update "${STEAMAPPID}" +quit"
 
 # Change rcon port on first launch, because the default config overwrites the commandline parameter (you can comment this out if it has done it's purpose)
 sed -i -e 's/Port=21114/'"Port=${RCONPORT}"'/g' "${STEAMAPPDIR}/SEGame/ServerConfig/Rcon.cfg"
