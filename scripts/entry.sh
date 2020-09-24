529e5ed4030759731faf48068e8df39f6b6e876d
Nathan Moritz <nateim3@gmail.com>
2020-09-24 12:22:15 -0400

    Added string literals to shell script

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-"${STEAMCMDDIR}/steamcmd.sh +login anonymous +force_install_dir "${STEAMAPPDIR}" +app_update "${STEAMAPPID}" +quit"
+bash "${STEAMCMDDIR}/steamcmd.sh" '+login anonymous' \
+				'+force_install_dir' "${STEAMAPPDIR}" \
+				'+app_update' "${STEAMAPPID}" \
+				'+quit'
 
 # Change rcon port on first launch, because the default config overwrites the commandline parameter (you can comment this out if it has done it's purpose)
 sed -i -e 's/Port=21114/'"Port=${RCONPORT}"'/g' "${STEAMAPPDIR}/SEGame/ServerConfig/Rcon.cfg"
 
 wine "${STEAMAPPDIR}/SpaceEngineersDedicated.exe"
-Port="${PORT}" \
+bash 	Port="${PORT}" \
 		QueryPort="${QUERYPORT}" \
 		RCONPORT="${RCONPORT}" \
 		FIXEDMAXPLAYERS="${FIXEDMAXPLAYERS}" \
