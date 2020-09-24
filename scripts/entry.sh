1b197917c53ed22aa4dca68f0bba559f04767e50
Nathan Moritz <nateim3@gmail.com>
2020-09-24 12:15:44 -0400

    Testing shell script

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-bash "${STEAMCMDDIR}/steamcmd.sh +login anonymous +force_install_dir "${STEAMAPPDIR}" +app_update "${STEAMAPPID}" +quit"
+"${STEAMCMDDIR}/steamcmd.sh +login anonymous +force_install_dir "${STEAMAPPDIR}" +app_update "${STEAMAPPID}" +quit"
 
 # Change rcon port on first launch, because the default config overwrites the commandline parameter (you can comment this out if it has done it's purpose)
 sed -i -e 's/Port=21114/'"Port=${RCONPORT}"'/g' "${STEAMAPPDIR}/SEGame/ServerConfig/Rcon.cfg"
 
 wine "${STEAMAPPDIR}/SpaceEngineersDedicated.exe"
-bash 	Port="${PORT}" \
+Port="${PORT}" \
 		QueryPort="${QUERYPORT}" \
 		RCONPORT="${RCONPORT}" \
 		FIXEDMAXPLAYERS="${FIXEDMAXPLAYERS}" \
